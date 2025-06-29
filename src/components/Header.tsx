2067d3b5318aacb803b3b4ba746e6c60bf58e27f
Dylan BARILLOT <dylan.barillo@gmail.com>
2025-06-29 18:22:29 +0200

    fix : modif titre

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="https://cdn.mgig.fr/2020/06/mg-9c5e379e-560d-498d-97d1_accroche.jpg" className="h-8 rounded-md" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">o'Meteo</span>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">o'Meteos</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
